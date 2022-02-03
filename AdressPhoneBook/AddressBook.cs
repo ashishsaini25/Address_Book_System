cd01e1ff237e93d7f107e2f4287c5da52088daa9
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-03 22:55:21 +0530

    [Ashish] Add: Contact details Added

---
+++ AdressPhoneBook/AddressBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressPhoneBook
+{
+    public class AddressBook
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set;}
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phonenumber { get; set; }
+        public string Email { get; set; }
+        public void AddContact()
+        {
+            Console.WriteLine("Enter the First Name");
+            this.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            this.LastName = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            this.Address = Console.ReadLine();
+            Console.WriteLine("Enter the City");
+            this.City = Console.ReadLine();
+            Console.WriteLine("Enter the State");
+            this.State = Console.ReadLine();
+            Console.WriteLine("Enter the Zip");
+            this.Zip = Console.ReadLine();
+            Console.WriteLine("Enter the PhoneNumber");
+            this.Phonenumber = Console.ReadLine();
+            Console.WriteLine("Enter the Email");
+            this.Email = Console.ReadLine();
+
+            Console.WriteLine("Contact Added\n");
+
+
+
+        }
+    }
+}
--- AdressPhoneBook/Program.cs
+++ AdressPhoneBook/Program.cs
@@ -10,6 +10,8 @@ namespace AdressPhoneBook
     {
         static void Main(string[] args)
         {
+            AddressBook book = new AddressBook();
+            book.AddContact();
         }
     }
 }
